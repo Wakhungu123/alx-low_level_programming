6daa20c3d2f7642b5ba4df8566dbf0beb390f02d
Melvin <melvinnafula24@gmail.com>
2023-02-22 02:56:47 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,22 @@
 #include "putchar.h"
 
 /**
- * 0x02-puthcar
- * project 1
- **/
+ * 0x02-putchar
+ *
+ * Always return 0
+ *
+ */
 
 int main(void)
 {
     int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-
+    
     int c, size;
-
+    
     size = sizeof(str)/sizeof(int);
-
     for(c = 0; c < size; c++){
        _putchar(str[c]);
     }
-
     _putchar('\n');
-
     return 0;
-
 }
