ab938eccb805c287075c3ce4969311263b73740f
Melvin <melvinnafula24@gmail.com>
2023-02-22 22:10:40 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - prints the _alphabet in lowercase.
+ *
+ * Return: Always return 0 (success)
+ *
+ */
+int main(void)
+{
+        for(int'a'=97;'a'<='122';'a'++);
+	{
+		//printf("%d\n", 'a');
+
+        int c, size;
+
+        size = sizeof(str) / sizeof(int);
+        for (c = 0; c < size; c++)
+        {
+                _putchar(str[c]);
+        }
+        _putchar('\n');
+        return (0);
+}
