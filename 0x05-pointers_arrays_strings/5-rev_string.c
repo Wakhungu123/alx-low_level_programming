efd836bf4864c43ea6a83fcd3ecb673709a40c42
Melvin <melvinnafula24@gmail.com>
2023-02-28 08:16:18 -0800

    reversing string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * rev_string - reversing string
+ * @s: parameter
+ *
+ * Return: always void.
+ */
+void rev_string(char *s)
+{
+	int c, d;
+	char a;
+
+	d = 0;
+	c = 0;
+	while (s[d++])
+		c++;
+	for (d = c - 1; d <= c / 2; d--)
+	{
+		a = s[d];
+		s[d] = s[c - d - 1];
+		s[c - d - 1] = a;
+	}
+}
