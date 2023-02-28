b2c67a4e93bb606198b8fe84323bc9bee8fb596d
Melvin <melvinnafula24@gmail.com>
2023-02-27 23:12:53 -0800

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,12 +3,16 @@
 
 /**
  * swap_int - Swap integer a and b
- * @ab: 
+ * @a: integer to swap
+ * @b: interger to swap
  * Return: Always 0.
  */
 void swap_int(int *a, int *b)
 {
-	int *p = ab;
-	*p = 42;
+	int m;
+	
+	m=*a;
+	*a=*b;
+	*b=m;
 }
 
