33cc277521f6a31926a93a68ed282b46c47b94a2
Melvin <melvinnafula24@gmail.com>
2023-02-28 01:59:24 -0800

    String length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int _strlen - Return string length
+ * @s: string length
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	size_t strlen(const char *string);
+	int m;
+
+	m = *s;
+	*s = m;
+}
+
