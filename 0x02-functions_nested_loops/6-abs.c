442a9fa1a0a34c77012bc9a55e656068b34a39fc
Melvin <melvinnafula24@gmail.com>
2023-03-03 15:15:49 -0800

    absolute integers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - compute the absolute value of integer
+ * @r: parameter
+ *
+ * Return: r
+ *
+ */
+int _abs(int r)
+{
+	int a;
+
+	if (r < 0)
+	{
+		a = (r * -1);
+		return (a);
+	}
+	return (r);
+}
